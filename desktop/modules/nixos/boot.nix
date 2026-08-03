709feaba79ca559de9f27df22e297fda424a5845
5aether <186336097+5aether@users.noreply.github.com>
2026-08-03 15:14:42 +0200

    added zen kernel, new-lg4ff and appimage compatibility + tweaks

--- desktop/modules/nixos/boot.nix
+++ desktop/modules/nixos/boot.nix
@@ -6,6 +6,27 @@
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages_zen;
+    # OBS Camera
+    kernelModules = [ "v4l2loopback" ];
+    extraModulePackages = with config.boot.kernelPackages; [
+      v4l2loopback
+    ];
+    extraModprobeConfig = ''
+      options v4l2loopback devices=1 video_nr=1 close_delay=0 card_label="OBS Virtual Camera" exclusive_caps=1
+    '';
+    # Fix for some steam games
+    kernel.sysctl = {
+      "vm.max_map_count" = 2147483642;
+    };
+  };
+
+  # Better FFB Support
+  hardware.new-lg4ff.enable = true;
+
+  # App Image Compatibility
+  programs.appimage = {
+    enable = true;
+    binfmt = true;
   };
 }
