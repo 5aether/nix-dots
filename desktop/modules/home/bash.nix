30a19f33f80009c35a21a327c8917a252733b476
5aether <186336097+5aether@users.noreply.github.com>
2026-08-05 01:55:37 +0200

    changed the noctalia alias

--- desktop/modules/home/bash.nix
+++ desktop/modules/home/bash.nix
@@ -8,7 +8,7 @@
       "nixp" = "nvim ~/nix-dots/desktop/modules/home/packages.nix";
       "nixe" = "sudo nix-collect-garbage -d; sudo nixos-rebuild switch --flake ~/nix-dots#desktop";
       "nixr" = "sudo nixos-rebuild switch --flake ~/nix-dots#desktop";
-      "noctalia-update" = "noctalia config export full >> ~/nix-dots/desktop/modules/home/noctalia/config.toml; sudo nixos-rebuild switch --flake ~/nix-dots#desktop";
+      "noctalia-config" = "rm -rf ~/nix-dots/desktop/modules/home/noctalia/config.toml; noctalia config export full >> ~/nix-dots/desktop/modules/home/noctalia/config.toml; sudo nixos-rebuild switch --flake ~/nix-dots#desktop";
     };
 
     initExtra = ''
