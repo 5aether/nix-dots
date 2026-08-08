662e8acf109a784f66c38435fdb8fd27d66cb30c
5aether <186336097+5aether@users.noreply.github.com>
2026-08-08 02:39:21 +0200

    added dns

--- desktop/modules/nixos/networking.nix
+++ desktop/modules/nixos/networking.nix
@@ -2,6 +2,14 @@
   networking = {
     hostName = "desktop";
     wireless.enable = true;
+    nameservers = [ "192.168.1.200" ];
     networkmanager.enable = true;
+  };
+
+  services.resolved = {
+    enable = true;
+    settings.Resolve.FallbackDNS = [
+      "1.1.1.1"
+    ];
   };
 }
