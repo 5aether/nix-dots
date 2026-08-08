116b43c7ce6fcf2d4d3d23f8b9eb0a7dcee2f3fa
5aether <186336097+5aether@users.noreply.github.com>
2026-08-08 03:44:28 +0200

    added some things for samba

--- desktop/modules/nixos/services.nix
+++ desktop/modules/nixos/services.nix
@@ -1,4 +1,15 @@
 {
-  services.gvfs.enable = true;
-  services.udisks2.enable = true;
+  services = {
+    gvfs.enable = true;
+    udisks2.enable = true;
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
+    samba-wsdd = {
+      enable = true;
+      openFirewall = true;
+    };
+  };
 }
