8f6df7f2592c911085d31add552c22575892e1f3
5aether <186336097+5aether@users.noreply.github.com>
2026-08-26 00:45:06 +0200

    changed again lol

--- desktop/modules/nixos/networking.nix
+++ desktop/modules/nixos/networking.nix
@@ -2,12 +2,10 @@
   networking = {
     hostName = "desktop";
     networkmanager.enable = true;
+    nameservers = [ "192.168.1.200" ];
   };
 
   services.resolved = {
-    enable = true;
-    dns = [ "192.168.1.200" ];
-    domains = [ "~." ];
-    dnsovertls = "false";
+    enable = false;
   };
 }
