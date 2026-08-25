bac1a298a96c9f17f853cf14856de6310b13cc50
5aether <186336097+5aether@users.noreply.github.com>
2026-08-26 01:38:46 +0200

    dns

--- desktop/modules/nixos/networking.nix
+++ desktop/modules/nixos/networking.nix
@@ -1,6 +1,15 @@
 {
   networking = {
     hostName = "desktop";
+    wireless.enable = true;
     networkmanager.enable = true;
+  };
+
+  services.resolved = {
+    enable = true;
+    settings.Resolve = {
+      DNS = [ "192.168.1.200" ];
+      FallbackDNS = [ "1.1.1.1" ];
+    };
   };
 }
